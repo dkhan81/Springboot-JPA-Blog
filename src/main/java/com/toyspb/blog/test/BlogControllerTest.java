7e696ca5a12691583669570c2b03564bf6201b89
DK Han <thislove81@gmail.com>
2021-02-03 17:18:51 +0900

    gitignore add

--- src/main/java/com/toyspb/blog/test/BlogControllerTest.java
+++ src/main/java/com/toyspb/blog/test/BlogControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 // 스프링이 com.toyspb.blog 패키지 이하를 스캔해서 모든 파일을 메모리에 new 하는 것은 아니구요.
 // 특정 어노테이션이 붙어있는 클래스 파일들을 new 해서(IoC) 스프링 컨테이너에서 관리해 줍니다.
 @RestController
-public class BlogControllerTest {
+public class BlogControllerTest { 
 
     // http://localhost:8080/test/hello
     @GetMapping("/test/hello")
